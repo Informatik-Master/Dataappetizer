46310aa52f6f90a3d8b52f3d178e6dfbe860049e
Matthias Vonend <matthias.vonend@outlook.de>
2023-04-13 11:31:17 +0200

    get latest

--- proto2-backend/index.ts
+++ proto2-backend/index.ts
@@ -24,10 +24,9 @@ app.get('/datapoints', async function (req, res) {
   res.json(Items);
 });
 
-
 app.get('/datapoints-vin', async function (req, res) {
   let vin = req.query['vin']?.toString()!;
-  const {Items}=await dynamoDbClient.send(
+  const { Items } = await dynamoDbClient.send(
     new QueryCommand({
       TableName: process.env['DATAPOINT_TABLE'],
       ScanIndexForward: false,
@@ -43,6 +42,31 @@ app.get('/datapoints-vin', async function (req, res) {
   res.json(Items);
 });
 
+app.get('/datapoints-vin-latest', async function (req, res) {
+  const dataPoints = ['geolocation', 'mileage'];
+  let vin = req.query['vin']?.toString()!;
+
+  const queries = dataPoints.map(async (dataPoint) => {
+    const searchKey = `${vin}#${dataPoint}`;
+    return dynamoDbClient.send(
+      new QueryCommand({
+        TableName: process.env['DATAPOINT_TABLE'],
+        IndexName: process.env['DATAPOINT_TABLE_DATAPOINT_INDEX'],
+        ScanIndexForward: false,
+        KeyConditionExpression: 'vinWithDataPointName = :searchKey',
+        ExpressionAttributeValues: {
+          ':searchKey': {
+            S: searchKey,
+          },
+        },
+        Limit: 1,
+      }),
+    )
+  }
+  );
+
+  res.json((await Promise.all(queries)).flatMap((v) => v.Items));
+});
 
 app.get('/connections', async function (req, res) {
   const { Items } = await dynamoDbClient.send(
