e7515c1830ee8a88bd3d99e037230cea796c8f88
Shadrach Arulrajah <shadrach@live.de>
2023-04-21 13:15:03 +0200

    basic spinner in carList

--- Proto1/src/app/pages/car/car.component.ts
+++ Proto1/src/app/pages/car/car.component.ts
@@ -26,9 +26,18 @@ export class CarComponent implements OnInit {
      }, 1000)
     this.socket.fromEvent('carList').pipe(map((data) => data), tap((data) => console.log(data))).subscribe((data: any) => {
       console.log(data);
+      hideloader();
       this.dataSource= data;
     })
+
+
+    function hideloader() {
+      let spinner = document.getElementById('loading');
+      spinner!.style.display = 'none';
   }
+  }
+
+  
 
   onRowClicked(row: any) {
     console.log('Row clicked: ', row);
